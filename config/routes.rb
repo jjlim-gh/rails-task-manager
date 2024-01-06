c115207f5e35ef7136010cd2879574d40d5e1c09
Justin Limlengco <jjmlimlengco@gmail.com>
2024-01-06 16:22:58 +0900

    Completed the main functions and refactored the application code

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,16 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
+  # Read all tasks
+  get 'tasks', to: 'tasks#index', as: :tasks
+  # Add new task
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  # Show a task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # Edit a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  # Delete a task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
